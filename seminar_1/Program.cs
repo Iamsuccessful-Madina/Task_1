af01d4ca9dedf838467118e7fd7b036beebfd419
Madina <madinazhekshenbaeva97@mail.com>
2024-03-22 18:04:54 +0600

    Сохраняем

--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -6,7 +6,7 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if(number1 == number2 * number2)
+if (number1 == number2 * number2)
 {
     Console.WriteLine($"Квадрат числа {number2} равен {number1}");
 }
@@ -23,3 +23,17 @@ while (notN <= N)
     Console.Write($"{notN} ");
     notN++;
 }
+
+// Напишите программу, которая принимает на вход трехзначное целое число и 
+// на выходе показывает сумму первой и последней цифры этого числа
+
+int n = Convert.ToInt32(Console.ReadLine());
+int first = n % 10;
+int second = n / 10 % 10;
+int third = n / 100;
+int sum = first + third;
+Console.WriteLine(first);
+Console.WriteLine(second);
+Console.WriteLine(third);
+
+Console.WriteLine(sum);
