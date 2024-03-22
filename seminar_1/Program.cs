f47881f662c990f992711886486c966877dcb9a9
Madina <madinazhekshenbaeva97@mail.com>
2024-03-22 17:20:32 +0600

    new file

---
+++ seminar_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает два целых числа и проверяет, 
+// является ли первое число квадратом второго.
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2 * number2)
+{
+    Console.WriteLine($"Квадрат числа {number2} равен {number1}");
+}
+else Console.WriteLine("Квадрат числа {0} НЕ равен {1}", number2, number1);
+
+// Напишите программу, которпая на вход принимает целое число N, 
+// а на выходе показывает все целые числа в промежутке от -N до N 
+
+int N = Convert.ToInt32(Console.ReadLine());
+int notN = -N;
+
+while (notN <= N)
+{
+    Console.Write($"{notN} ");
+    notN++;
+}
